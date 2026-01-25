1c26126f10f8926376606cde28927ecf4a86a321
Rafeef2Rizq <rfyfrzq7@gmail.com>
2026-01-25 20:18:02 +0200

    login

--- routes/admin.php
+++ routes/admin.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\admin\{CategoryController, AdminController};
 use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
 
 
-Route::group(['prefix' => LaravelLocalization::setLocale()], function () {
+Route::prefix(LaravelLocalization::setLocale())->middleware(['auth', 'isAdmin', 'verified'])->group(function () {
     Route::prefix('admin')->name('admin.')->group(function () {
         Route::get('/', [AdminController::class, 'index'])->name('index');
         Route::resource('categories', CategoryController::class);
